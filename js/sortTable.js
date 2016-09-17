8f7c3f29606062e3cd54f5f75b654119b41a4950
Trishla Shah <trishla92@hotmail.com>
2016-09-16 22:29:14 -0400

    code to sort the table

---
+++ js/sortTable.js
@@ -0,0 +1,42 @@
+function sortTable(table, col, reverse) {
+    var tb = table.tBodies[0],
+        tr = Array.prototype.slice.call(tb.rows, 0),
+        i;
+    reverse = -((+reverse) || -1);
+    if(col === 2){
+        tr = tr.sort(function (a, b) {
+            if( parseFloat(a.cells[col].textContent.trim()) < parseFloat(b.cells[col].textContent.trim())){
+                return reverse * -1;
+            }else
+                return reverse * 1;
+        });
+    }else{
+        tr = tr.sort(function (a, b) {
+            return reverse
+                * (a.cells[col].textContent.trim()
+                        .localeCompare(b.cells[col].textContent.trim())
+                );
+        });
+    }
+    for(i = 0; i < tr.length; ++i) tb.appendChild(tr[i]);
+
+}
+
+function makeSortable(table) {
+    var th = table.tHead, i;
+    th && (th = th.rows[0]) && (th = th.cells);
+    if (th)
+        i = th.length;
+    else
+        return;
+    while (--i >= 2) (function (i) { // since we are only sorting last 2 columns
+        var dir = 1;
+        th[i].addEventListener('click', function () {sortTable(table, i, (dir = 1 - dir))});
+    }(i));
+}
+
+function makeAllSortable(parent) {
+    parent = parent || document.body;
+    var t = parent.getElementsByTagName('table'), i = t.length;
+    while (--i >= 0) makeSortable(t[i]);
+}
