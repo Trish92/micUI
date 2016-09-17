1bf90454dcb2c2d6c592c670a746d794e169624a
Trishla Shah <trishla92@hotmail.com>
2016-09-16 22:31:45 -0400

    remove parsing

--- js/jsonParser.js
+++ js/jsonParser.js
@@ -29,7 +29,7 @@ function showData() {
             '<tr>' +
             '<td><img src="'+elem.image +'"/>'+elem.title +'</td>' + // unpublished article header
             '<td>'+elem.profile.first_name+' '+elem.profile.last_name +'</td>' + //author
-            '<td>'+parseFloat(elem.words)+'</td>' + // words
+            '<td>'+elem.words+'</td>' + // words
             '<td>'+elem.publish_at+'</td>' + //submitted time
             '</tr>';
         $('table#tbl TBODY').append(htmlString);
